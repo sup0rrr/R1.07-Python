2723f7b2561204232b24e1dc12d3e2c9015f597b
sup0rrr <147592717+sup0rrr@users.noreply.github.com>
2023-10-26 09:57:42 +0200

    Delete tp1exo3.py

--- tp1exo3.py
+++
@@ -1,5 +0,0 @@
-jour =int(input('Quel est le jour ?'))
-heure =int(input('Quel est lheure ?'))
-minute=int(input('Quel est la minute ?'))
-
-print('minute depuis le debut =', jour * 1440 + heure * 60 + minute)
